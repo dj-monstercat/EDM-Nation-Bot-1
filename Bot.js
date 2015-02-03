5c29bd941de301811ea968fedcb75647189cc91c
LastIdi0t <ivanry@live.nl>
2015-02-03 21:55:51 +0100

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -1,3 +1,10 @@
+/**
+ *Copyright 2014 Yemasthui
+ *Modifications (including forks) of the code to fit personal needs are allowed only for personal use and should refer back to the original source.
+ *This software is not for profit, any extension, or unauthorised person providing this software is not authorised to be in a position of any monetary gain from this use of this software. Any and all money gained under the use of the software (which includes donations) must be passed on to the original author.
+ */
+
+
 (function () {
 
     API.getWaitListPosition = function(id){
@@ -221,7 +228,7 @@
             filterChat: true,
             etaRestriction: false,
             welcome: true,
-            opLink: null,
+            opLink: "http://goo.gl/SGFnOh",
             rulesLink: null,
             themeLink: null,
             fbLink: "https://www.facebook.com/pages/EDM-Nation/1550760605164057",
