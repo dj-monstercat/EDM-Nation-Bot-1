74d20d36cf0e1677f8533ef4bb860ad53585a407
LastIdi0t <ivanry@live.nl>
2015-03-03 16:08:49 +0100

    Update BetaBot.js

--- BetaBot.js
+++ BetaBot.js
@@ -2921,6 +2921,19 @@
                     }
                 }
             },
+            
+            twitterCommand: {
+                command: 'twitter',
+                rank: 'user',
+                type: 'exact',
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!trapbot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                            API.sendChat(subChat(trapbot.chat.twitter, {link: trapbot.settings.twitter));
+                    }
+                }
+            },
 
             youtubeCommand: {
                 command: 'youtube',
